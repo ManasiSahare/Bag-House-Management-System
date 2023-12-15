c64881e66fb0ebbaaeb1caaba2523284f21b8f98
ManasiSahare <153941645+ManasiSahare@users.noreply.github.com>
2023-12-15 23:12:00 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,30 @@
+<section class="footer">
+
+   <div class="box-container">
+
+   
+   <div class="box">
+         <h3><i class="fas fa-coffee"></i>   Drink -IT</h3>
+         <p>Drink-IT Is A Highly Scaleable And Versatile Coffee Business Solution.Introducing Drinkit-Coffee Shop To Have A Coffee At Your Place With Fast Service .</p>
+         
+      </div>
+
+
+
+      
+
+      <div class="box">
+         <h3></h3>
+         <p> <i class="fas fa-phone"></i> +123-456-7890 </p>
+         <p> <i class="fas fa-phone"></i> +111-222-3333 </p>
+         <p> <i class="fas fa-envelope"></i> Drinkit@gmail.com </p>
+         <p> <i class="fas fa-map-marker-alt"></i> mumbai, india - 400104 </p>
+      </div>
+
+     
+
+   </div>
+
+   <p class="credit">Website is Desinged  by <span>mr.Nikhil and ms. Diksha</span> </p>
+
+</section>
